ca8ca3cf0e51f520c391b200904b2d045378106b
Wenan <wenanlee@users.noreply.github.com>
2019-04-14 17:05:59 +0800

    Add files via upload

---
+++ Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        SqlHelper.Insatance.SetConnStr("120.27.188.117", "root", "YJsqlpwd132", "vr");
+        foreach (var item in SqlHelper.Insatance.GetTables())
+        {
+            print(item);
+        }
+    }
+}
